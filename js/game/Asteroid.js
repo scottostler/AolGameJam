7eb096e4cfaef542d5efb855aec967d87fa92bfd
Scott Ostler <scottostler@gmail.com>
2013-03-14 09:08:52 -0400

    Add different scores for killing asteroids

--- js/game/Asteroid.js
+++ js/game/Asteroid.js
@@ -8,6 +8,7 @@ Asteroid = function(game)
     this.offsetY = 0;
 	this.level = 0;
 	this.myRotation = 0;
+	this.pointsForKilling = 0;
 };
 
 
@@ -19,12 +20,14 @@ Asteroid.prototype =
 		this.level = 1;
 		this.myRotation = 120;
 		this.type = "asteroid_small";
+		this.pointsForKilling = 100;
 		if(rand > 0.6 && totalTime >10)
 		{
 			this.myRotation = 40;
 			this.asteroidSpeed = 300;
 			this.type = "asteroid_big";
 			this.level = 2;
+			this.pointsForKilling = 200;
 		}
 		if(rand > 0.8 && this.mGame.maxHuge < 2 && totalTime > 30)
 		{
@@ -32,6 +35,7 @@ Asteroid.prototype =
 			this.asteroidSpeed = 200;
 			this.type = "asteroid_giant";
 			this.level = 3;
+			this.pointsForKilling = 400;
 			this.mGame.maxHuge++;
 		}
 		var x = this.mGame.mScreenManager.XFromPercentage(xPos);
--- js/game/Bullet.js
+++ js/game/Bullet.js
@@ -10,147 +10,133 @@ Bullet.prototype =
     Setup: function(spaceship)
     {
         var yPos = spaceship.y - spaceship.height / 2;
-		this.percentFull = spaceship.powerCharge/spaceship.maxPowerCharge;
+        this.percentFull = spaceship.powerCharge/spaceship.maxPowerCharge;
         this.type = "shot";
-		this.sound = "Laser1";
-		this.speed =400;
-		var row = 1;
-		var col = 4;
-		var frames = 4;
-		var framerate = 24;
-		this.level = 1;
-		if(this.percentFull > 0.33)
-		{
-			this.type = "three_beam";
-			this.sound = "Laser2";
-			this.speed =	500;
-			row = 1;
-			col = 3;
-			frames = 3;
-			framerate = 24;
-			this.level = 2;
-		}
-		if(this.percentFull > 0.66)
-		{
-			this.type = "energy_ball";
-			this.sound = "Laser3";
-			this.speed =	800;
-			row = 1;
-			col = 4;
-			frames = 4;
-			framerate = 24;
-			this.level = 3;
-		}
-		
+        this.sound = "Laser1";
+        this.speed =400;
+        var row = 1;
+        var col = 4;
+        var frames = 4;
+        var framerate = 24;
+        this.level = 1;
+        if(this.percentFull > 0.33)
+        {
+            this.type = "three_beam";
+            this.sound = "Laser2";
+            this.speed =    500;
+            row = 1;
+            col = 3;
+            frames = 3;
+            framerate = 24;
+            this.level = 2;
+        }
+        if(this.percentFull > 0.66)
+        {
+            this.type = "energy_ball";
+            this.sound = "Laser3";
+            this.speed =    800;
+            row = 1;
+            col = 4;
+            frames = 4;
+            framerate = 24;
+            this.level = 3;
+        }
+        
         Bullet.superclass.Setup.call(this, spaceship.x, yPos, this.type,"spaceship");
-		
-		this.LoadAnimation("moving",this.type,row,col,frames,framerate,true);
-		this.PlayAnimation("moving");
-		this.mGame.audioManager.Play({id:this.sound, loop:false});
+        
+        this.LoadAnimation("moving",this.type,row,col,frames,framerate,true);
+        this.PlayAnimation("moving");
+        this.mGame.audioManager.Play({id:this.sound, loop:false});
         this.offsetX = 0;
         this.offsetY = 0;
         this.radius = this.width/2;
-		this.soundDelay = 0.255;
-		this.maxSoundDelay = 0.255;
+        this.soundDelay = 0.255;
+        this.maxSoundDelay = 0.255;
         return this;
     },
 
     subclassUpdate: function(elapsedTime)
     {
-    	this.y -= this.speed * elapsedTime * this.mGame.speedMultiplier;
-        this.detectAsteroidCollisions();
+        this.y -= this.speed * elapsedTime * this.mGame.speedMultiplier;
+        detectFirstCollision(this, this.mGame.getAsteroids(), this.collisionCallback.bind(this));
 
         if (this.y + this.radius < 0) {
             this.markForRemoval();
         }
-		this.soundDelay -= elapsedTime;
+        this.soundDelay -= elapsedTime;
     },
+    
+    collisionCallback: function(bullet, enemy) {
+        if (bullet.level < enemy.level) {
+            bullet.visible = false;
+            bullet.markForRemoval();
+        } else {
+            if (bullet.level == enemy.level) {
+                bullet.visible = false;
+                bullet.markForRemoval();
+            }
 
-    detectAsteroidCollisions: function() {	
-        var eIndex = detectFirstCollision(this, this.mGame.getAsteroids(),this.callBack.bind(this));
+            enemy.visible = false;
+            enemy.markForRemoval();
+            this.createAsteroidDeath(enemy.x/this.mGame.Width(),enemy.y/this.mGame.Height(),enemy.type);
+            this.mGame.score += enemy.pointsForKilling;
 
-        if (eIndex >= 0) {
-            this.mGame.score += 100;
+            if (enemy.level == 3) {
+                this.mGame.maxHuge--;
+            }
+        } 
+    },
+    
+    createAsteroidDeath: function (xPos,yPos,type)
+    {
+        var anim = CreateScreenUI(this.mGame,xPos,yPos,"ast_death","asteroid");
+        
+        var img = "ast_death";
+        var row = 1;
+        var col = 6;
+        var frames = 6;
+        var framerate = 24;
+        var soundName = "Explosion1_01";
+        var scaleMe = 1;
+        if(type == "asteroid_big")
+        {
+            img = "big_ast_death";
+            row = 3;
+            col = 5;
+            frames = 15;
+            framerate = 24;
+            var rand = Math.floor(Math.random()*2);
+            if(rand == 0)
+            {
+                soundName = "Explosion2_01";
+            }
+            else
+            {
+                soundName = "Explosion2_02";
+            }
+            var scaleMe = 2;
+        }
+        else if(type == "asteroid_giant")
+        {
+            img = "giant_ast_death";
+            row = 5;
+            col = 5;
+            frames = 25;
+            framerate = 24;
+            soundName = "Explosion3_01";
+            var scaleMe = 10;
+            
+        }
+        anim.LoadAnimation("exploding",img,row,col,frames,framerate,false);
+        anim.PlayAnimation("exploding",this.markForRemoval.bind(anim));
+        anim.scaleX = scaleMe;
+        anim.scaleY = scaleMe;
+        if(this.soundDelay < 0)
+        {
+            this.mGame.audioManager.Play({id:soundName, loop:false});
+            this.soundDelay = this.maxSoundDelay;
         }
     },
-	
-	 callBack: function(bullet, enemy) {
-            if (bullet.level < enemy.level) {
-                bullet.visible = false;
-                bullet.markForRemoval();
-				//var shot_fizz = CreateScreenUI(this.mGame,xPos,yPos,"ast_death","asteroid");
-            }
-			else if(bullet.level == enemy.level)
-			{
-                bullet.visible = false;
-                bullet.markForRemoval();
-				enemy.visible = false;
-				enemy.markForRemoval();
-				if(enemy.level == 3)
-				{
-					this.mGame.maxHuge--;
-				}
-				this.createAsteroidDeath(enemy.x/this.mGame.Width(),enemy.y/this.mGame.Height(),enemy.type);
-			}
-			else
-			{
-				enemy.visible = false;
-				enemy.markForRemoval();
-				this.createAsteroidDeath(enemy.x/this.mGame.Width(),enemy.y/this.mGame.Height(),enemy.type);
-			}
-			
-    },
-	
-	createAsteroidDeath: function (xPos,yPos,type)
-	{
-		var anim = CreateScreenUI(this.mGame,xPos,yPos,"ast_death","asteroid");
-		
-		var img = "ast_death";
-		var row = 1;
-		var col = 6;
-		var frames = 6;
-		var framerate = 24;
-		var soundName = "Explosion1_01";
-		var scaleMe = 1;
-		if(type == "asteroid_big")
-		{
-			img = "big_ast_death";
-			row = 3;
-			col = 5;
-			frames = 15;
-			framerate = 24;
-			var rand = Math.floor(Math.random()*2);
-			if(rand == 0)
-			{
-				soundName = "Explosion2_01";
-			}
-			else
-			{
-				soundName = "Explosion2_02";
-			}
-			var scaleMe = 2;
-		}
-		else if(type == "asteroid_giant")
-		{
-			img = "giant_ast_death";
-			row = 5;
-			col = 5;
-			frames = 25;
-			framerate = 24;
-			soundName = "Explosion3_01";
-			var scaleMe = 10;
-			
-		}
-		anim.LoadAnimation("exploding",img,row,col,frames,framerate,false);
-		anim.PlayAnimation("exploding",this.markForRemoval.bind(anim));
-		anim.scaleX = scaleMe;
-		anim.scaleY = scaleMe;
-		if(this.soundDelay < 0)
-		{
-			this.mGame.audioManager.Play({id:soundName, loop:false});
-			this.soundDelay = this.maxSoundDelay;
-		}
-	},
-}
+};
 
 extend(Bullet,TGE.ScreenEntity);
